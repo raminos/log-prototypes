7896bc1c7cb5e54387d7392120aeec9c23f8fd4e
ramin <rx.azhdari@gmail.com>
2022-12-21 13:40:09 +0100

    enh: finish parsePrototypes function

--- src/lib/parse.spec.ts
+++ src/lib/parse.spec.ts
@@ -2,17 +2,392 @@ import test from 'ava';
 
 import { parsePrototypes } from './parse';
 
-test('Test function', (t) => {
+const objectParsedObject = {
+  constructorName: 'Object',
+  properties: [
+    {
+      name: '__defineGetter__',
+      type: { name: 'function', description: '__defineGetter__' },
+    },
+    {
+      name: '__defineSetter__',
+      type: { name: 'function', description: '__defineSetter__' },
+    },
+    {
+      name: 'hasOwnProperty',
+      type: { name: 'function', description: 'hasOwnProperty' },
+    },
+    {
+      name: '__lookupGetter__',
+      type: { name: 'function', description: '__lookupGetter__' },
+    },
+    {
+      name: '__lookupSetter__',
+      type: { name: 'function', description: '__lookupSetter__' },
+    },
+    {
+      name: 'isPrototypeOf',
+      type: { name: 'function', description: 'isPrototypeOf' },
+    },
+    {
+      name: 'propertyIsEnumerable',
+      type: { name: 'function', description: 'propertyIsEnumerable' },
+    },
+    {
+      name: 'toString',
+      type: { name: 'function', description: 'toString' },
+    },
+    {
+      name: 'valueOf',
+      type: { name: 'function', description: 'valueOf' },
+    },
+    {
+      name: '__proto__',
+      type: { name: 'undefined', description: undefined },
+    },
+    {
+      name: 'toLocaleString',
+      type: { name: 'function', description: 'toLocaleString' },
+    },
+  ],
+  parsedPrototype: undefined,
+};
+
+test('Test constructor function with all common types as properties', (t) => {
+  class TestClass {}
   function Test(this: any) {
     this.a = 'World';
+    this.b = 123;
+    this.c = false;
+    this.d = Symbol('Hello');
+    this.e = function printHello() {
+      console.log('Hello');
+    };
+    this.f = function () {
+      console.log("I'm so anonymous");
+    };
+    this.g = BigInt('9007199254740991');
+    this.h = new Date('2022-12-21');
+    this.i = /ab*c/g;
+    this.j = new Map([
+      [1, 'Hello'],
+      [2, 'World'],
+    ]);
+    this.k = new Set(['Hello', 'World']);
+    this.l = { a: 'Hello', b: 'World' };
+    this.m = Object.getPrototypeOf(new Map());
+    this.n = Object.getPrototypeOf('Hello World');
+    this.o = new TestClass();
+    this.p = undefined;
+    this.q = null;
+    this.r = NaN;
+    this.s = Infinity;
   }
   const testObj: any = new (Test as any)();
   const result = parsePrototypes(testObj);
-  console.log(JSON.stringify(result, null, 1));
-  t.is(result, undefined);
-  // Test.prototype.getA = function () {
-  //   return this.a;
-  // };
-  // const testObj2: any = new (Test as any)();
-  // t.is(parsePrototypes(testObj2), undefined);
+  t.deepEqual(result, {
+    constructorName: undefined,
+    properties: [
+      {
+        name: 'a',
+        type: { name: 'string', description: '"World"' },
+      },
+      {
+        name: 'b',
+        type: { name: 'number', description: '123' },
+      },
+      {
+        name: 'c',
+        type: { name: 'boolean', description: 'false' },
+      },
+      {
+        name: 'd',
+        type: { name: 'symbol', description: 'Hello' },
+      },
+      {
+        name: 'e',
+        type: { name: 'function', description: 'printHello' },
+      },
+      {
+        name: 'f',
+        type: { name: 'function', description: 'anonymous' },
+      },
+      {
+        name: 'g',
+        type: {
+          description: '9007199254740991',
+          name: 'bigint',
+        },
+      },
+      {
+        name: 'h',
+        type: {
+          description: '2022-12-21T00:00:00.000Z',
+          name: 'Date',
+        },
+      },
+      {
+        name: 'i',
+        type: {
+          description: '/ab*c/g',
+          name: 'RegExp',
+        },
+      },
+      {
+        name: 'j',
+        type: {
+          description: '2',
+          name: 'Map',
+        },
+      },
+      {
+        name: 'k',
+        type: {
+          description: '2',
+          name: 'Set',
+        },
+      },
+      {
+        name: 'l',
+        type: {
+          description: '2',
+          name: 'object',
+        },
+      },
+      {
+        name: 'm',
+        type: {
+          description: '11',
+          name: 'Map.prototype',
+        },
+      },
+      {
+        name: 'n',
+        type: {
+          description: '50',
+          name: 'String.prototype',
+        },
+      },
+      {
+        name: 'o',
+        type: {
+          description: undefined,
+          name: 'TestClass',
+        },
+      },
+      {
+        name: 'p',
+        type: {
+          description: undefined,
+          name: 'undefined',
+        },
+      },
+      {
+        name: 'q',
+        type: {
+          description: undefined,
+          name: 'null',
+        },
+      },
+      {
+        name: 'r',
+        type: {
+          description: 'NaN',
+          name: 'number',
+        },
+      },
+      {
+        name: 's',
+        type: {
+          description: 'Infinity',
+          name: 'number',
+        },
+      },
+    ],
+    parsedPrototype: {
+      constructorName: 'Test',
+      properties: [],
+      parsedPrototype: objectParsedObject,
+    },
+  });
+});
+
+test('Test simple class', (t) => {
+  class Test {
+    a = 'Hello';
+    b() {
+      console.log('World');
+    }
+  }
+  t.deepEqual(parsePrototypes(new Test()), {
+    constructorName: undefined,
+    properties: [
+      {
+        name: 'a',
+        type: {
+          name: 'string',
+          description: '"Hello"',
+        },
+      },
+    ],
+    parsedPrototype: {
+      constructorName: 'Test',
+      properties: [
+        {
+          name: 'b',
+          type: {
+            name: 'function',
+            description: 'b',
+          },
+        },
+      ],
+      parsedPrototype: objectParsedObject,
+    },
+  });
+});
+
+test('Test class with inheritance', (t) => {
+  class MetaTest {
+    c = 'Meta Hello';
+    d() {
+      console.log('I love being meta.');
+    }
+  }
+  class Test extends MetaTest {
+    a = 'Hello';
+    b() {
+      console.log('World');
+    }
+  }
+
+  t.deepEqual(parsePrototypes(new Test()), {
+    constructorName: undefined,
+    properties: [
+      {
+        name: 'c',
+        type: { name: 'string', description: '"Meta Hello"' },
+      },
+      {
+        name: 'a',
+        type: {
+          name: 'string',
+          description: '"Hello"',
+        },
+      },
+    ],
+    parsedPrototype: {
+      constructorName: 'Test',
+      properties: [
+        {
+          name: 'b',
+          type: {
+            name: 'function',
+            description: 'b',
+          },
+        },
+      ],
+      parsedPrototype: {
+        constructorName: 'MetaTest',
+        properties: [
+          {
+            name: 'd',
+            type: {
+              name: 'function',
+              description: 'd',
+            },
+          },
+        ],
+        parsedPrototype: objectParsedObject,
+      },
+    },
+  });
+});
+
+test('Test object with added prototype', (t) => {
+  const testPrototype = {
+    a: 'Meta Hello',
+    b() {
+      console.log('World');
+    },
+  };
+  const test = {
+    a: 'Hello',
+    __proto__: testPrototype,
+  };
+
+  t.deepEqual(parsePrototypes(test), {
+    constructorName: undefined,
+    properties: [
+      {
+        name: 'a',
+        type: { name: 'string', description: '"Hello"' },
+      },
+    ],
+    parsedPrototype: {
+      constructorName: undefined,
+      properties: [
+        {
+          name: 'a',
+          type: { name: 'string', description: '"Meta Hello"' },
+        },
+        { name: 'b', type: { name: 'function', description: 'b' } },
+      ],
+      parsedPrototype: objectParsedObject,
+    },
+  });
+});
+
+test('Test built in type', (t) => {
+  t.deepEqual(parsePrototypes(1), {
+    constructorName: undefined,
+    properties: [],
+    parsedPrototype: {
+      constructorName: 'Number',
+      properties: [
+        {
+          name: 'toExponential',
+          type: {
+            name: 'function',
+            description: 'toExponential',
+          },
+        },
+        {
+          name: 'toFixed',
+          type: {
+            name: 'function',
+            description: 'toFixed',
+          },
+        },
+        {
+          name: 'toPrecision',
+          type: {
+            name: 'function',
+            description: 'toPrecision',
+          },
+        },
+        {
+          name: 'toString',
+          type: {
+            name: 'function',
+            description: 'toString',
+          },
+        },
+        {
+          name: 'valueOf',
+          type: {
+            name: 'function',
+            description: 'valueOf',
+          },
+        },
+        {
+          name: 'toLocaleString',
+          type: {
+            name: 'function',
+            description: 'toLocaleString',
+          },
+        },
+      ],
+      parsedPrototype: objectParsedObject,
+    },
+  });
 });
--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,56 +1,123 @@
-import { capitalize } from '.';
-
 type ParsedObject = {
-  name: string | undefined;
-  fields: Field[];
-  prototype: ParsedObject | undefined;
+  constructorName: string | undefined;
+  properties: Property[];
+  parsedPrototype: ParsedObject | undefined | string;
 };
 
-type Field = {
+type ParsedType = {
   name: string;
-  value: string | ParsedObject;
+  description: string | undefined;
 };
 
-function getValueName(obj: any): string {
-  if (['string', 'boolean', 'number'].includes(typeof obj)) {
-    return `${capitalize(typeof obj)} (${obj})`;
-  } else if (typeof obj === 'undefined') {
-    return `Undefined`;
-  } else if (Array.isArray(obj)) {
-    return `Array (length: ${obj.length})`;
+type Property = {
+  name: string;
+  type: ParsedType;
+};
+
+type Options = {
+  includeConstructor: boolean;
+};
+
+function isPlainObject(obj: Record<string, any>) {
+  if (
+    (obj.prototype === null ||
+      Object.getPrototypeOf(obj) === Object.prototype) &&
+    !Object.getOwnPropertyNames(obj).includes('constructor')
+  ) {
+    return true;
   }
-  return capitalize(typeof obj);
+  return false;
 }
 
-function getFieldFromDescriptorEntry([name, descriptor]: [
+function getParsedType(obj: unknown): ParsedType {
+  if (['boolean', 'string'].includes(typeof obj)) {
+    return { name: typeof obj, description: JSON.stringify(obj) };
+  } else if (typeof obj === 'number') {
+    return {
+      name: 'number',
+      description: Number.isNaN(obj) ? 'NaN' : obj.toString(),
+    };
+  } else if (obj === 'undefined') {
+    return { name: 'undefined', description: undefined };
+  } else if (typeof obj === 'bigint') {
+    return { name: 'bigint', description: obj.toString() };
+  } else if (obj === null) {
+    return { name: 'null', description: undefined };
+  } else if (typeof obj === 'symbol') {
+    return { name: 'symbol', description: obj.description };
+  } else if (typeof obj === 'function') {
+    return {
+      name: 'function',
+      description: obj.name === '' ? 'anonymous' : obj.name,
+    };
+  } else if (Array.isArray(obj)) {
+    return { name: 'Array', description: obj.length.toString() };
+  } else if (obj instanceof Date) {
+    return { name: 'Date', description: obj.toISOString() };
+  } else if (obj instanceof RegExp) {
+    return { name: 'RegExp', description: `/${obj.source}/${obj.flags}` };
+  } else if (obj instanceof Map) {
+    return { name: 'Map', description: obj.size.toString() };
+  } else if (obj instanceof Set) {
+    return { name: 'Set', description: obj.size.toString() };
+  } else if (obj && isPlainObject(obj)) {
+    return {
+      name: 'object',
+      description: Object.getOwnPropertyNames(obj).length.toString(),
+    };
+  } else if (obj && Object.getOwnPropertyNames(obj).includes('constructor')) {
+    return {
+      name: `${obj.constructor.name}.prototype`,
+      description: Object.getOwnPropertyNames(obj).length.toString(),
+    };
+  } else if (
+    obj &&
+    Object.getPrototypeOf(obj) &&
+    Object.getOwnPropertyDescriptor(Object.getPrototypeOf(obj), 'constructor')
+  ) {
+    return { name: obj.constructor.name, description: undefined };
+  }
+
+  return { name: typeof obj, description: undefined };
+}
+
+function getPropertyFromDescriptorEntry([name, descriptor]: [
   string,
-  TypedPropertyDescriptor<any> & PropertyDescriptor
-]): Field {
+  TypedPropertyDescriptor<unknown> & PropertyDescriptor
+]): Property {
   return {
     name,
-    value: getValueName(descriptor.value),
+    type: getParsedType(descriptor.value),
   };
 }
 
-export function getFields(obj: any) {
+export function getProperties(obj: unknown, options: Options) {
   const descriptors = Object.getOwnPropertyDescriptors(obj);
-  const descriptorEntries = Object.entries(descriptors);
-  return descriptorEntries.map(getFieldFromDescriptorEntry);
+  let descriptorEntries = Object.entries(descriptors);
+  if (!options.includeConstructor) {
+    descriptorEntries = descriptorEntries.filter(
+      ([name]) => name !== 'constructor'
+    );
+  }
+  return descriptorEntries.map(getPropertyFromDescriptorEntry);
 }
 
-export function parsePrototypes(obj: any): ParsedObject | undefined {
+export function parsePrototypes(
+  obj: unknown,
+  options: Options = { includeConstructor: false }
+): ParsedObject | undefined {
   if (!obj) {
     return undefined;
   }
 
   let name;
   if (Object.getOwnPropertyNames(obj).includes('constructor')) {
-    name = obj.constructor?.name;
+    name = obj.constructor.name;
   }
 
   return {
-    name,
-    fields: getFields(obj),
-    prototype: parsePrototypes(Object.getPrototypeOf(obj)),
+    constructorName: name,
+    properties: getProperties(obj, options),
+    parsedPrototype: parsePrototypes(Object.getPrototypeOf(obj), options),
   };
 }
